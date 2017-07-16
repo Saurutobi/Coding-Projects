2272d1ae11233af640b92c46b2545b8ad6f52fc4
Marcel Englmaier <saurutobi@gmail.com>
2017-07-16 14:54:00 -0700

    Goal #1: Importing Json

--- CampaignAnalyzer/Program.cs
+++ CampaignAnalyzer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using CampaignAnalyzer.DataModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CampaignAnalyzer
 {
@@ -6,6 +10,54 @@ namespace CampaignAnalyzer
 	{
 		static void Main(string[] args)
 		{
+			List<Campaign> campaigns = ImportCampaignsFromFile();
+			List<Placement> placements = ImportPlacementsFromFile();
+
+			if(campaigns.Count == 0 || placements.Count == 0)
+			{
+				Console.WriteLine("Data files are invalid. Please check format and retry");
+			}
+			else
+			{
+
+			}
+
+			Console.WriteLine("Press any key to exit");
+			Console.ReadLine();
 		}
+
+		#region Helper Methods
+
+		public static List<Campaign> ImportCampaignsFromFile()
+		{
+			Console.WriteLine("Reading Campaigns");
+			List<Campaign> campaigns = new List<Campaign>();
+			using (StreamReader file = new StreamReader(@"c:\testdata\campaigns.json"))
+			{
+				string currentLine;
+				while ((currentLine = file.ReadLine()) != null)
+				{
+					campaigns.Add(JsonConvert.DeserializeObject<Campaign>(currentLine));
+				}
+			}
+			return campaigns;
+		}
+
+		public static List<Placement> ImportPlacementsFromFile()
+		{
+			Console.WriteLine("Reading Placements");
+			List<Placement> placements = new List<Placement>();
+			using (StreamReader file = new StreamReader(@"c:\testdata\placements.json"))
+			{
+				string currentLine;
+				while ((currentLine = file.ReadLine()) != null)
+				{
+					placements.Add(JsonConvert.DeserializeObject<Placement>(currentLine));
+				}
+			}
+			return placements;
+		}
+
+		#endregion
 	}
 }
--- CampaignAnalyzer/README.txt
+++ CampaignAnalyzer/README.txt
@@ -1,2 +1,6 @@
 To Run:
 	campaigns.json and placements.json must go in c:\testdata\
+	
+To Build:
+	Open Solution, and Rebuild Solution.
+		Caveats: Make sure you have NugetRestore enabled. This project needs Newtonsoft.Json
