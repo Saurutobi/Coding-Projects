fe00794058f0beeb32cbf77d1b40466775e67c3d
Marcel Englmaier <saurutobi@gmail.com>
2017-07-16 19:48:14 -0700

    Goal #3: Allocate nominal funds to campaign

--- CampaignAnalyzer/Program.cs
+++ CampaignAnalyzer/Program.cs
@@ -10,6 +10,7 @@ namespace CampaignAnalyzer
 	{
 		static void Main(string[] args)
 		{
+			// Goal 1
 			List<Campaign> campaigns = ImportCampaignsFromFile();
 			List<Placement> placements = ImportPlacementsFromFile();
 
@@ -19,13 +20,20 @@ namespace CampaignAnalyzer
 			}
 			else
 			{
-				Console.WriteLine("Getting Estimated cumulative clicks for each Placement at day 2, 4, 6");
-				foreach(var placement in placements)
-				{
-					Console.WriteLine($"PlacementID: {placement.id}, Estimated cumulative clicks for day 2: {GetEstimatedClicksForDay(placement, 2)}");
-					Console.WriteLine($"PlacementID: {placement.id}, Estimated cumulative clicks for day 4: {GetEstimatedClicksForDay(placement, 4)}");
-					Console.WriteLine($"PlacementID: {placement.id}, Estimated cumulative clicks for day 6: {GetEstimatedClicksForDay(placement, 6)}");
-				}
+				// Goal 2
+				Goal2(placements);
+				Console.WriteLine("");
+
+				// Goal 3
+				Goal3(campaigns, placements);
+				Console.WriteLine("");
+
+
+
+
+
+
+
 			}
 
 			Console.WriteLine("Press any key to exit");
@@ -86,6 +94,57 @@ namespace CampaignAnalyzer
 			{
 				return clicks;
 			}
+		}
+
+		public static void Goal2(List<Placement> placements)
+		{
+			Console.WriteLine("Getting Estimated cumulative clicks for each Placement at day 2, 4, 6");
+			foreach (var placement in placements)
+			{
+				Console.WriteLine($"PlacementID: {placement.id}, Estimated cumulative clicks for day 2: {GetEstimatedClicksForDay(placement, 2)}");
+				Console.WriteLine($"PlacementID: {placement.id}, Estimated cumulative clicks for day 4: {GetEstimatedClicksForDay(placement, 4)}");
+				Console.WriteLine($"PlacementID: {placement.id}, Estimated cumulative clicks for day 6: {GetEstimatedClicksForDay(placement, 6)}");
+			}
+		}
+
+		public static void Goal3(List<Campaign> campaigns, List<Placement> placements)
+		{
+			int placementCounter = 0;
+			foreach(var campaign in campaigns)
+			{
+				if(placementCounter >= placements.Count)
+				{
+					placementCounter = 0;
+				}
+
+				// Make sure we get a placement that's in cost budget
+				decimal? cost = GetEstimatedClicksForDay(placements[placementCounter], campaign.lengthInDays) * placements[placementCounter].costPerClick;
+				int startingPlacementCounter = placementCounter;
+				while(cost == null || cost > campaign.budget)
+				{
+					placementCounter++;
+					if(placementCounter == startingPlacementCounter)
+					{
+						//Easiest way to end this right here
+						throw new Exception($"Could not find a campaign and placement match. Everything for Campaign {campaign.name} is too expensive");
+					}
+					else if (placementCounter >= placements.Count)
+					{
+						placementCounter = 0;
+					}
+					cost = GetEstimatedClicksForDay(placements[placementCounter], campaign.lengthInDays) * placements[placementCounter].costPerClick;
+				}
+
+				//Calculate and output
+				decimal? totalConversions = GetEstimatedClicksForDay(placements[placementCounter], campaign.lengthInDays) * placements[placementCounter].conversionsPerClick;
+				Console.WriteLine($"Using PlacementID: {placements[placementCounter].id} for Campaign: {campaign.name}.");
+				Console.WriteLine($"Daily cost is {cost / campaign.lengthInDays}.");
+				Console.WriteLine($"Total conversions: {totalConversions}");
+				Console.WriteLine($"Cost per conversion: {GetEstimatedClicksForDay(placements[placementCounter], campaign.lengthInDays) / totalConversions}");
+				Console.WriteLine($"Remaining Budget is {campaign.budget - cost}.{Environment.NewLine}");
+				placementCounter++;
+			}
+
 		}
 
 		#endregion
